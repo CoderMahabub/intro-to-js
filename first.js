4cee9eebf8809ba3df6f962bcfa89532ff9dcbed
CoderMahabub <codermahabub@gmail.com>
2021-07-31 23:56:05 +0600

    Simple Matha Calculation using js

--- first.js
+++ first.js
@@ -1,2 +1,7 @@
 var price = 15;
 var age = 29;
+var temperature = 28;
+var familyMember = 7;
+var myName = "Suzon";
+var myGf = "Afrin";
+var address = "Barishal";
---
+++ mathematical-operation.js
@@ -0,0 +1,19 @@
+var oniounPrice = 43;
+var eggPrice = 42;
+var eggQuantity = 7;
+eggPriceMultiplication = eggQuantity * eggPrice;
+var totalPrice = oniounPrice + eggPrice;
+var priceDifference = oniounPrice - eggPrice;
+// console.log(oniounPrice);
+// console.log(eggPrice);
+// console.log(oniounPrice + eggPrice);
+// console.log(totalPrice);
+// console.log(priceDifference);
+// console.log(oniounPrice - eggPrice);
+// console.log(eggPrice * eggQuantity);
+// console.log(eggPriceMultiplication);
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / oniounPrice;
+console.log(orangeQuantity);
+
