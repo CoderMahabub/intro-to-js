ac00dcfa8b7878ffdf5f051870897e38254c01fa
CoderMahabub <codermahabub@gmail.com>
2021-08-01 00:35:37 +0600

    Conversion from float or string to number

---
+++ conversion.js
@@ -0,0 +1,19 @@
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName + lastName;
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+var onionPrice = '42';
+var eggPrice = '41';
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+// integer 
+var sunglass = 3;
+//float
+var price = 99.99;
+var OniounPriceNumber = parseInt(onionPrice);
+console.log(OniounPriceNumber);
+var priceint = parseInt(price);
+console.log(priceint);
+var priceFloat = parseFloat(price);
+console.log(priceFloat);
