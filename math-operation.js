17ad22855b2ce767205cd2f06789dab7653830e3
CoderMahabub <codermahabub@gmail.com>
2021-08-01 00:18:36 +0600

    age++ and age--

---
+++ math-operation.js
@@ -0,0 +1,31 @@
+// Recap Last Lession 
+var priceOne = 31;
+var priceTwo = 10;
+var sum = priceOne + priceTwo;
+var difference = priceTwo - priceOne;
+var multiplication = priceOne * priceTwo;
+var division = priceTwo / priceOne;
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+//
+// var doublePrice = priceTwo * 20;
+// var newPrice = priceOne + 10;
+// var newPrice = priceOne + 10;
+// priceOne = priceOne + 10;
+priceOne += 10;
+priceTwo -= 2;
+
+console.log(priceOne);
+console.log(priceTwo);
+
+var age = 14;
+age = age + 1;
+// age += 1; or age++ are both same 
+console.log(age);
+age++;
+console.log(age);
+age--;
+age--;
+console.log(age);
